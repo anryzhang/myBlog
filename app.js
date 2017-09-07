d610fbcd728bead0b9f345b766550794d9e55740
ruizhang <zhang.rui@yuejian.com>
2017-09-07 22:08:14 +0800

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const cookieSession = require('cookie-session');
 const consolidate = require('consolidate');
-const expressRouter = require('express-router');
+
 
 const app = express();
 app.use(express.static(__dirname + '/static'));
