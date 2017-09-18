19d4dbaea846fad8e8a600e5b418da5d674c4af5
ruizhang <zhang.rui@yuejian.com>
2017-09-18 09:47:29 +0800

    update datetime

--- server/api/blog/index.js
+++ server/api/blog/index.js
@@ -23,8 +23,8 @@ router.get('/',(req,res)=>{
             res.status(200).json({status:500,msg:'mysql error'});
         }else{
 
-            console.log(result);
             result.forEach((cur)=>{
+                cur.post_time = (new Date(cur.post_time)).getTime();
                 if(cur.author == _sess_id){
                     cur.isOwner = true;
                 }else{
@@ -39,25 +39,35 @@ router.get('/',(req,res)=>{
 
 });
 
+let getTime = function () {
+    let checkLength = function(part) {
+        return (part < 10) ? '0' + part : part;
+    };
+
+    let date = new Date(),
+        year = date.getFullYear(),
+        month = checkLength(date.getMonth() + 1),
+        day = checkLength(date.getDate()),
+        hour = checkLength(date.getHours()),
+        minute = checkLength(date.getMinutes()),
+        second = checkLength(date.getSeconds());
+
+    return  year + '-' + month + '-' + day +' ' + hour + ':' + minute + ':' + second;
+}
+
 router.post('/add',(req,res)=>{
    let _sess_id = req.session['admin_id'];
    if(!_sess_id){
        res.status(200).json({status:400,msg:'请先登录!'});
        return false;
    }
-
    console.log(req.body);
-
-
    let title = req.body.title;
    let pic_src = req.body.picSrc;
    let summary = req.body.summary;
    let content = req.body.content;
-   let post_time  = (new Date()).getTime();
+   let post_time = getTime();
    let author = req.session['admin_id'];
-
-
-    console.log(post_time.length);
 
    if(!title || !summary || !content){
        res.status(200).json({status:300,msg:'字段不能为空!'});
