fe3ee273ca1dabc8ad058d86e1b51b9bbbbb831d
ruizhang <zhang.rui@yuejian.com>
2017-09-19 13:56:12 +0800

    update blog over

--- assets/admin/blog/index.js
+++ assets/admin/blog/index.js
@@ -9,120 +9,181 @@ const API = require('./../../apiList');
 const $ = require('jquery');
 
 const BlogModule = {
-    init(){
+    init() {
         let self = this;
         self.J_blogFrom = $(document).find('.J_blogFrom');
         self.J_blogList = $(document).find('.J_blogList');
 
+        self.result = [];
 
         self.handleing();
         self.getBlogList();
     },
-    handleing(){
+    handleing() {
         let self = this;
-        self.J_blogFrom.on('click','.J_save',(e)=>{
+        self.J_blogFrom.on('click', '.J_save', (e) => {
             e.preventDefault();
             e.stopPropagation();
 
-            self.addBlogData(()=>{
+            self.addBlogData(() => {
                 self.getBlogList();
             });
         });
 
-        self.J_blogList.on('click','.J_del',(e)=>{
+        self.J_blogList.on('click', '.J_del', (e) => {
             e.preventDefault();
             e.stopPropagation();
             let id = $(e.currentTarget).attr('data-id');
-            self.delBlogItem(id,()=>{
+            self.delBlogItem(id, () => {
                 self.getBlogList();
             });
 
-        })
+        });
+
+        self.J_blogList.on('click', '.J_update', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            let id = $(e.currentTarget).attr('data-objId');
+            console.log(id);
+            let obj = {};
+
+            for (let key in self.result) {
+                if (self.result[key].id == id) {
+                    obj = self.deepCopy(self.result[key]);
+                    break;
+                }
+            }
+
+            self.J_blogFrom.find('.J_title').val(obj.title);
+            self.J_blogFrom.find('.J_summary').val(obj.summary);
+            self.J_blogFrom.find('.J_content').val(obj.content);
+            self.J_blogFrom.find('.J_save').attr('data-id', obj.id);
+        });
+
+        self.J_blogFrom.on('click', '.J_cancel', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            self.J_blogFrom.find('.J_title').val('');
+            self.J_blogFrom.find('.J_summary').val('');
+            self.J_blogFrom.find('.J_content').val('');
+            self.J_blogFrom.find('.J_save').removeAttr('data-id');
+
+        });
 
     },
-    delBlogItem(id,cb){
+    delBlogItem(id, cb) {
         $.ajax({
-            url:API.delBlog,
-            method:'post',
-            data:{
-                id:id
+            url: API.delBlog,
+            method: 'post',
+            data: {
+                id: id
             },
-            dataType:'json',
-            success(res){
+            dataType: 'json',
+            success(res) {
                 console.log(res);
-                if(res.status == 100){
-                    cb&&cb();
+                if (res.status == 100) {
+                    cb && cb();
                 }
             }
         })
     },
-    addBlogData(cb){
+    addBlogData(cb) {
         let self = this;
+        let id = self.J_blogFrom.find('.J_save').attr('data-id');
+
+        let _url = id ? API.updateBlog : API.addBlog;
+
         let _data = {
-            title:self.J_blogFrom.find('.J_title').val(),
+            title: self.J_blogFrom.find('.J_title').val(),
             picSrc: '',
             summary: self.J_blogFrom.find('.J_summary').val(),
-            content: self.J_blogFrom.find('.J_content').val()
+            content: self.J_blogFrom.find('.J_content').val(),
+            id:id
         };
 
-        if(!_data.title || !_data.summary || !_data.content ){
+        if (!_data.title || !_data.summary || !_data.content) {
 
             alert('检查表单内容!');
             return false;
         }
 
         $.ajax({
-            url: API.addBlog,
+            url: _url,
             method: 'post',
             data: _data,
-            dataType:'json',
-            success(res){
+            dataType: 'json',
+            success(res) {
                 console.log(res);
-                if(res.status == 100){
+                if (res.status == 100) {
                     self.J_blogFrom.find('.J_title').val('');
                     self.J_blogFrom.find('.J_summary').val('');
                     self.J_blogFrom.find('.J_content').val('');
-                    cb&&cb();
+                    self.J_blogFrom.find('.J_save').removeAttr('data-id');
+                    cb && cb();
                 }
             }
         });
 
     },
-    getBlogList(){
+    deepCopy(obj) {
+        let self = this;
+        let result = obj;
+        let isArray = Array.isArray;
+        let isObject = function (obj) {
+            let type = typeof obj;
+            return type === 'function' || type === 'object' && !!obj;
+        }
+        if (isObject(obj)) {
+            result = obj;
+        }
+        if (isArray(obj)) {
+            result = obj.slice();
+        }
+        return result;
+    },
+    getBlogList() {
         let self = this;
         $.ajax({
             url: API.selectBlog,
-            method:'get',
-            data:{
-                t:$.now()
+            method: 'get',
+            data: {
+                t: $.now()
             },
             dataType: 'json',
-            success(res){
-                console.log(res);
-                if(res.status == 100){
+            success(res) {
+                // console.log(res);
+                if (res.status == 100) {
                     let _html = '';
                     let data = res.result;
-                    data.forEach((cur)=>{
-                        _html += `<div class="item">
-                                <span>${cur.id}</span>
-                                <span>${cur.title}</span>
-                                <span>${cur.summary}</span>
-                                ${self.returnFun(cur)}
-                                </div>`;
+                    self.result = res.result;
+
+                    data.forEach((cur) => {
+                        _html += `<section class="row_item">
+                                    <div class="item">${cur.id}</div>
+                                    <div class="item">${cur.title}</div>
+                                    <div class="item">${cur.summary}</div>
+                                    ${self.returnFun(cur)}
+                                </section>`;
                     });
 
                     self.J_blogList.empty().append($(_html));
 
+                }else if(res.status == 400){
+                    location.reload(true);
                 }
             }
         });
     },
-    returnFun(obj){
+    returnFun(obj) {
         let self = this;
-        if(obj.isOwner){
-            return `<span data-id="${obj.id}" class="J_del fun">删除</span>`;
-        }else{
-            return `<span></span>`
+
+        if (obj.isOwner) {
+            return `<div class="item"> 
+                        <span  data-id="${obj.id}" class="J_del fun">删除</span> 
+                        <span  data-objId="${obj.id}" class="J_update fun">更新</span>
+                    </div>`;
+        } else {
+            return `<div class="item"></div>`
         }
     }
 };
--- server/api/blog/index.js
+++ server/api/blog/index.js
@@ -9,13 +9,28 @@ const router = express.Router();
 
 const DB = require('./../DB');
 
+
+router.use((req,res,next)=>{
+
+    console.log(req.baseUrl);
+    if(req.baseUrl.slice(1,7) == 'server'){
+        //后台登录
+        let _sess_id = req.session['admin_id'];
+        if(!_sess_id && req.url != '/login'){
+            res.status(200).json({status:400,msg:'请先登录'});
+        }else{
+            next();
+        }
+
+    }else{
+        //前台登录
+    }
+
+});
+
 router.get('/',(req,res)=>{
 
-    let _sess_id = req.session['admin_id'];
-    if(!_sess_id){
-        res.status(200).json({status:400,msg:'请先登录!'});
-        return false;
-    }
+    let authorId = req.session['admin_id'];
 
     DB.query(`select * from blog_table`,(err,result)=>{
         if(err){
@@ -25,7 +40,8 @@ router.get('/',(req,res)=>{
 
             result.forEach((cur)=>{
                 cur.post_time = (new Date(cur.post_time)).getTime();
-                if(cur.author == _sess_id){
+                cur.update_time = cur.update_time ? (new Date(cur.update_time)).getTime():null;
+                if(cur.author == authorId){
                     cur.isOwner = true;
                 }else{
                     cur.isOwner = false;
@@ -56,11 +72,7 @@ let getTime = function () {
 }
 
 router.post('/add',(req,res)=>{
-   let _sess_id = req.session['admin_id'];
-   if(!_sess_id){
-       res.status(200).json({status:400,msg:'请先登录!'});
-       return false;
-   }
+
    console.log(req.body);
    let title = req.body.title;
    let pic_src = req.body.picSrc;
@@ -87,12 +99,34 @@ router.post('/add',(req,res)=>{
 
 });
 
-router.post('/del',(req,res)=>{
-    let _sess_id = req.session['admin_id'];
-    if(!_sess_id){
-        res.status(200).json({status:400,msg:'请先登录!'});
+router.post('/update',(req,res)=>{
+
+    let id = req.body.id;
+    let title = req.body.title;
+    let pic_src = req.body.picSrc;
+    let summary = req.body.summary;
+    let content = req.body.content;
+    let update_time = getTime();
+
+    if(!id || !title || !summary || !content){
+        res.status(200).json({status:300,msg:'相关字段不能为空'});
         return false;
     }
+
+    DB.query(`update blog_table set title='${title}' , summary='${summary}' , content='${content}',update_time = '${update_time}' where id='${id}'`,(err,result)=>{
+        if(err){
+            console.log(err);
+            res.status(200).json({status:500,msg:'mysql error'});
+        }else{
+            res.status(200).json({status:100,msg:'blog更新成功',result:result});
+        }
+    });
+
+
+});
+
+router.post('/del',(req,res)=>{
+    
     let id = req.body.id;
     DB.query(`delete from blog_table where id= '${id}'`,(err,result)=>{
        if(err){
